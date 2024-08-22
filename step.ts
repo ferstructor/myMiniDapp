d0e97286ff256fe66e51e20a1af3f47ae94a3356
FGU <>
2024-08-22 20:35:32 +0200

    susmuert

---
+++ step.ts
@@ -0,0 +1,48 @@
+import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { mnemonicToWalletKey } from "@ton/crypto";
+import { TonClient, WalletContractV4, internal } from "@ton/ton";
+
+async function main() {
+  // open wallet v4 (notice the correct wallet version here)
+  const mnemonic = "assume snake solution defense focus under burst medal enforce brush describe cricket stereo hurt satisfy veteran cinnamon furnace diamond radio sort leisure clock surround"; // your 24 secret words (replace ... with the rest of the words)
+  const key = await mnemonicToWalletKey(mnemonic.split(" "));
+  const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 });
+  console.log(wallet.address.toString({ testOnly: true }));
+  // initialize ton rpc client on testnet
+  const endpoint = await getHttpEndpoint({ network: "testnet" });
+  const client = new TonClient({ endpoint });
+
+  // Check if the wallet contract is deployed on the blockchain
+  // If it's not deployed, log a message and exit the function
+
+  // send 0.05 TON to EQA4V9tF4lY2S_J-sEQR7aUj9IwW-Ou2vJQlCn--2DLOLR5e
+  const walletContract = client.open(wallet);
+  const seqno = await walletContract.getSeqno();
+  await walletContract.sendTransfer({
+    secretKey: key.secretKey,
+    seqno: seqno,
+    messages: [
+      internal({
+        to: "EQA4V9tF4lY2S_J-sEQR7aUj9IwW-Ou2vJQlCn--2DLOLR5e",
+        value: "0.05", // 0.05 TON
+        body: "Hello cyberpunks", // optional comment
+        bounce: false,
+      })
+    ]
+  });
+
+  // wait until confirmed
+  let currentSeqno = seqno;
+  while (currentSeqno == seqno) {
+    console.log("waiting for transaction to confirm...");
+    await sleep(1500);
+    currentSeqno = await walletContract.getSeqno();
+  }
+  console.log("transaction confirmed!");
+}
+
+main();
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
